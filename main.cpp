9e2434226db8cf13b58c261425b58e4c09cfc329
k022g0059@g.neec.ac.jp <k022g0059@g.neec.ac.jp>
2023-04-19 14:33:34 +0900

    00_03

--- main.cpp
+++ main.cpp
@@ -1,9 +1,136 @@
 #include <Windows.h>
+#include <cstdint>
+#include <string>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <format>
+#include "ConvertString.h"
+
+#pragma comment(lib, "d3d12")
+#pragma comment(lib, "dxgi.lib")
+
+//ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+//文字列を格納する
+std::string str0{ "STRING!!!" };
+
+//整数を文字列にする
+std::string str1{ std::to_string(10) };
+
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
+//変数から方を推論してくれる
+//Log(std::format("enemyHp:{}, texturePath:{}\n", enemyHp, texturePath));
+
+//string->wstring
+std::wstring ConvertString(const std::string& str);
+//wstring->string
+std::string ConvertString(const std::wstring& str);
 
 //Windowsあぷりでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	//出力ウィンドウへの文字出力
-	OutputDebugStringA("Hello,DirectX!\n");
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd) {
+
+	//DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+	//HRESULTはWindows系のエラーコードであり
+	// 関数が成功したかどうかをSUCCEEDEDまくろで判定できる
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	//初期化の根本的な部分でエラーが出た場合はプログラムが間違っているか、どうにもできない場合が多いのでassertにしておく
+	assert(SUCCEEDED(hr));
+
+	//使用するアダプタ用の変数。最初にnullptrを入れておく
+	IDXGIAdapter4* useAdapter = nullptr;
+	//良い順にアダプタを頼む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i) {
+		//アダプターの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));//取得できないのは一大事
+		//ソフトウェアアダプタでなければ採用!
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			//採用したアダプタの情報をログに出力。wstringの方なので注意
+			//Log(std::format(L"Use Adapater:{}\n", adapterDesc.Description));
+			break;
+		}
+		useAdapter = nullptr;//ソフトウェアアダプタの場合は見なかったことにする
+	}
+
+	//適切なアダプタが見つからなかったので起動できない
+	assert(useAdapter != nullptr);
+
+	//クライアント領域のサイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名(なんでも良い)
+	wc.lpszClassName = L"CG2WindowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+	//ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
+	
+	//クライアント領域を元に実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	//ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,     // 利用するクラス名
+		L"CG2WindowClass",	  // タイトルバーの文字(なんでも良い)
+		WS_OVERLAPPEDWINDOW,  // よく見るウィンドウスタイル
+		CW_USEDEFAULT,		  // 表示x座標
+		CW_USEDEFAULT,		  // 表示y座標
+		wrc.right - wrc.left, // ウィンドウ横幅
+		wrc.bottom - wrc.top, // ウィンドウ縦幅
+		nullptr,			  // 親ウィンドウハンドル
+		nullptr,			  // メニューハンドル
+		wc.hInstance,		  // インスタンスハンドル
+		nullptr);			  // オプション
+	//ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+	
+
+	MSG msg{};
+	while (msg.message != WM_QUIT) {
+		//Windowにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			//ゲームの処理
+
+		}
+	}
 
 	return 0;
 }
